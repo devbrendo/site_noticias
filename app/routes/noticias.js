5bdff0593394794f998eea2f17e2b0dca84c0bcd
devbrendo <93737945+devbrendo@users.noreply.github.com>
2022-04-20 00:48:07 -0300

    3

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,18 @@
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
-        res.render("noticias/noticias")
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'portal_noticias'
+        });//parametros passados em json
+
+        connection.query('select * from noticias', function (erro, result) {
+            res.send(result);
+        });
+        //res.render("noticias/noticias")
 
     });
 };
