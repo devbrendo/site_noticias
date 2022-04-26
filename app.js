29aeb6161812be02e1c9e06995f70752409b177e
devbrendo <93737945+devbrendo@users.noreply.github.com>
2022-04-25 21:04:17 -0300

    4

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
-
+/*var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaHome = require('./app/routes/home')(app);
-
 var rotaFormInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
+*/ //desconsiderando rotas ao utilizar consign
+
 
 app.listen(3000, function () {
     console.log("servidor ON");
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,13 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
+
+    var connection = dbConnection();
+
     app.get('/noticias', function (req, res) {
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'portal_noticias'
-        });//parametros passados em json
-
         connection.query('select * from noticias', function (erro, result) {
-            res.send(result);
+            res.render("noticias/noticias", {noticias : result});
         });
-        //res.render("noticias/noticias")
 
     });
 };
