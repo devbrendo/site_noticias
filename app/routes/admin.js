b1a41ac0ffd9e3c49baf47191cede0b18e565559
devbrendo <93737945+devbrendo@users.noreply.github.com>
2022-05-05 23:59:29 -0300

    33

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render("admin/form_add_noticia");
+        res.render("admin/form_add_noticia", {validacao:{}, noticia : {}});
 
     });
     app.post('/noticias/salvar', function (req, res) {
@@ -18,7 +18,7 @@ module.exports = function (app) {
         var erros = req.validationErrors();
 
         if(erros){
-            res.render("admin/form_add_noticia", {validacao : erros});
+            res.render("admin/form_add_noticia", {validacao : erros, noticia : noticia});
             return;
         }
 
